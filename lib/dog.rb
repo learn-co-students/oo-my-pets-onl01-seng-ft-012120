32810c30cdd9e7ba7bbb8bbb4bcd009195d8e270
reddevilcero <reddevil_cero@hotmail.com>
2020-02-03 16:51:50 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,23 @@
 class Dog
-  # code goes here
+  @@all = []
+  attr_accessor :owner, :mood
+  attr_reader :name
+
+  def initialize(name, owner)
+    @name = name
+    self.owner = owner
+    self.mood = 'nervous'
+    self.save
+  end
+
+  def save
+    @@all << self
+  end
+
+  def self.all 
+    @@all
+  end
+
+
+
 end
