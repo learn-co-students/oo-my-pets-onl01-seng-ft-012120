d7af8a849758b67e2adc96bc8ad0526fa4780940
Erica Schimmel <ericaschimmel01@gmail.com>
2020-02-05 16:17:18 +0000

    Done.

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,3 +1,23 @@
 class Cat
   # code goes here
+  attr_accessor :mood, :owner
+  attr_reader :name
+  
+  @@all = []
+  
+  def initialize(name, owner)
+    @name = name 
+    @owner = owner
+    @mood = "nervous"
+    save
+  end 
+  
+  def save 
+    @@all << self
+  end
+  
+  def self.all 
+    return @@all 
+  end 
+  
 end
