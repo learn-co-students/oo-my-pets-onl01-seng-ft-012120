78423625a742f8cc291b6652f898a0ee620df06a
poignant-writer-1163 <edmundopierre@yahoo.com>
2020-02-10 00:36:47 +0000

    Done.

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,3 +1,59 @@
 class Cat
   # code goes here
+  attr_accessor :nervous 
+  
+  
+  @@all = []
+  
+  def initialize(name,owner)
+    
+        @name = name
+ 
+        @owner = owner
+        @nervous = "nervous"
+        @@all << self
+  end
+  
+  #def name=(name) 
+    
+    # @name = name
+     
+ # end
+  
+  def name
+    
+    @name
+    
+  end
+  
+  def owner
+    
+    @owner
+    
+  end
+  
+  def owner=(owner)
+    
+     @owner = owner
+    
+  
+  end
+  
+  def mood
+    
+   @nervous
+  end
+  
+   def mood=(nervous)
+    
+   @nervous="playful"
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  #def cats 
+  #self.all.count
+  #end
 end
--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,55 @@
+
+require "pry"
+
 class Owner
   # code goes here
+  
+ # attr_accessor
+ 
+ @@all = []
+  def initialize(name)
+    
+    @name= name
+    @species ="human"
+    @@all << self 
+    
+  end
+  def name
+    
+    @name
+    
+  end
+  
+  def species
+    
+    @species
+    
+  end
+  
+  def say_species
+    
+    "I am a #{@species}."
+  end
+  
+  def self.all 
+    
+    @@all
+    
+  end
+  
+  def self.count
+    
+    @@all.count
+  end
+  
+  def self.reset_all
+    
+    @@all.clear
+    
+  end
+  
+  def cats 
+  Cat.all.select{|cat_instance| cat_instance.owner== self}
+  #binding.pry
+  end
 end
